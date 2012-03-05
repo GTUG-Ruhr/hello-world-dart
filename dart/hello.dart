a1f6a72b5b2d76607cdd41eb785243e65e7b1d2a
Matthias Kluth <matthias@kluth.me>
2012-03-05 16:06:09 +0100

    Added timing

--- dart/hello.dart
+++ dart/hello.dart
@@ -1,10 +1,12 @@
 #import('dart:dom');
 
 void main () {
-  
+  var date = new Date.now();
+  print("Entry: ${date}");
   HTMLElement span = window.document.createElement('span');
   span.id = 'hellocontainer';
   span.innerHTML = 'hello, world.';
   window.document.body.appendChild(span);
-
+  date = new Date.now();
+  print("Exit: ${date}");
 }
